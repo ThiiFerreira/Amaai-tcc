2f489a7786f40d61ab0ad51dc237c1a935ab75cf
Thiago da Silva Ferreira <thiago.ferreira.p2001@gmail.com>
2022-09-09 23:26:14 -0300

    0

--- teste.tcc/UsuarioAPP/Controllers/LoginController.cs
+++ teste.tcc/UsuarioAPP/Controllers/LoginController.cs
@@ -39,6 +39,7 @@ namespace UsuariosApi.Controllers
         [HttpPost("/efetua-reset")]
         public IActionResult ResetaSenhaUsuario(EfetuaResetRequest request)
         {
+
             Result resultado = _loginService.ResetaSenhaUsuario(request);
             if (resultado.IsFailed) return Unauthorized(resultado.Errors);
             return Ok(resultado.Successes);
