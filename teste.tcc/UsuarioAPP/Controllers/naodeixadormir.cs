694f48adf9f955b711df31f96d5a455aa4d40341
Thiago da Silva Ferreira <thiago.ferreira.p2001@gmail.com>
2022-10-06 14:49:43 -0300

    alterando naodeixadormir

--- teste.tcc/UsuarioAPP/Controllers/naodeixadormir.cs
+++ teste.tcc/UsuarioAPP/Controllers/naodeixadormir.cs
@@ -12,16 +12,19 @@ namespace UsuariosApi.Controllers
     [Route("{controller}")]
     public class naodeixadormir : ControllerBase
     {
+        static int i = 0;
+
         [HttpGet]
         public void fazRequisicao()
         {
-                Console.WriteLine("Oi acordei");
-                int milliseconds = 10000;
-                Thread.Sleep(milliseconds);
-                Console.WriteLine("Oi sou eu de novo");
-                WebRequest request = WebRequest.Create("https://app-tcc-amai-producao.herokuapp.com/naodeixadormir");
-                request.Method = "GET";
-                var response = (HttpWebResponse)request.GetResponse();
+            i=i+1;
+            Console.WriteLine($"Oi acordei pela {i}° vez");
+            int milliseconds = 60000;
+            Thread.Sleep(milliseconds);
+            //WebRequest request = WebRequest.Create("https://app-tcc-amai-producao.herokuapp.com/naodeixadormir");
+            WebRequest request = WebRequest.Create("https://localhost:6001/naodeixadormir");
+            request.Method = "GET";
+            var response = (HttpWebResponse)request.GetResponse();
         }
     }
 }
