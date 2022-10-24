1b831eb7c067f30af3dc4f86f919a48834857aad
Thiago da Silva Ferreira <thiago.ferreira.p2001@gmail.com>
2022-10-24 10:21:31 -0300

    alterando forma de loop para não deixar a aplicação dormir

--- teste.tcc/UsuarioAPP/Controllers/naodeixadormir.cs
+++ teste.tcc/UsuarioAPP/Controllers/naodeixadormir.cs
@@ -14,13 +14,33 @@ namespace UsuariosApi.Controllers
     {
 
         [HttpGet]
-        public void fazRequisicao()
+        public IActionResult fazRequisicao()
+        {
+            Console.WriteLine("Criando cronometro para chamar loop");
+            criaCronometro(60000);
+            Console.WriteLine("Cronometro criado");
+
+            return NoContent();
+        }
+
+        private void criaCronometro(double tempo)
+        {
+            var cronometro = new System.Timers.Timer();
+            cronometro.Enabled = false;
+            cronometro.Interval = tempo;
+            cronometro.AutoReset = false;
+            cronometro.Elapsed += async (sender, e) => realizaLoopInfinito();
+            cronometro.Start();
+        }
+
+        private void realizaLoopInfinito()
         {
             int i = 1;
             while (i != 0)
             {
                 
                 Console.WriteLine($"Oi acordei pela {i}° vez");
+
                 i = i + 1;
                 int milliseconds = 900000;
                 Thread.Sleep(milliseconds);
